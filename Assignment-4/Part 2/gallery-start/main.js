f23a060ca9881e06723d9cbe9ec9ca965f869cfd
Mhtprk <mohit.parekh1@dcmail.ca>
2023-11-24 14:57:22 -0500

    Second Change
    Declaring the alternative names for the images in-case the images did not display the text will be displayed.

--- Assignment-4/Part 2/gallery-start/main.js
+++ Assignment-4/Part 2/gallery-start/main.js
@@ -5,14 +5,18 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+
 const imageFilename = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+
+/* Declaring the alternative text for each image file */
 
 const alternativeTexts = {
     'pic1.jpg': 'Closeup of a blue human eye',
-    'pic2.jpg': '',
-}
-
-/* Declaring the alternative text for each image file */
+    'pic2.jpg': 'Beautiful art',
+    'pic3.jpg': 'Attractive flowers',
+    'pic4.jpg': 'Painting showing the egyptians',
+    'pic5.jpg': 'Butterfly on leaf',
+};
 
 /* Looping through images */
 
