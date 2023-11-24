ee494dd8f303de90b5936368018f5e75b8819009
Mhtprk <mohit.parekh1@dcmail.ca>
2023-11-24 16:18:43 -0500

    Third Change
    Created update and collision detect function inside the class ball.

--- Assignment-4/Part 3/main.js
+++ Assignment-4/Part 3/main.js
@@ -35,4 +35,31 @@ class Ball{
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
   }
+
+  update() {
+    if (this.x + this.size >= width || this.x - this.size <= 0) {
+      this.velX = -this.velX;
+    }
+
+    if (this.y + this.size >= height || this.y - this.size <= 0) {
+      this.velY = -this.velY;
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
 }
