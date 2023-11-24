33842563d27c54af632b4d433ac6cfa0926cb8af
Mhtprk <mohit.parekh1@dcmail.ca>
2023-11-24 16:53:01 -0500

    Third Change
    Defining the evil circle.

--- Assignment-4/Part 4/main-finished.js
+++ Assignment-4/Part 4/main-finished.js
@@ -66,7 +66,7 @@ class Ball extends Shape{
 
    collisionDetect() {
       for (const ball of balls) {
-         if (!(this === ball)) {
+         if (!(this === ball) && ball.exists) {
             const dx = this.x - ball.x;
             const dy = this.y - ball.y;
             const distance = Math.sqrt(dx * dx + dy * dy);
@@ -112,3 +112,32 @@ function loop() {
 }
 
 loop();
+
+class EvilCircle extends Shape {
+   constructor(x, y) {
+      super(x, y, 20, 20);
+      this.size = 10;
+      this.color = 'white';
+      this.exists = true;
+      this.lineWidth = 3;
+
+      window.addEventListener('keydown', (e) => {
+         switch (e.key){
+            case 'a':
+               this.x -= this.velX;
+               break;
+            case 'd':
+               this.x += this.velX;
+               break;
+            case 'w':
+               this.y -= this.velY;
+               break;
+            case 's':
+               this.y += this.velY;
+               break;
+         }
+      });
+   }
+
+   draw
+}
